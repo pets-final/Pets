c5ef115e968762c8c8f492664036b706dd376e14
brahimbensalah <brahimbensalah17@gmail.com>
2024-05-10 09:46:17 +0100

    done

--- client/src/screens/Adopt/AdoptList.js
+++ client/src/screens/Adopt/AdoptList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Image, StatusBar, FlatList, KeyboardAvoidingView, TouchableOpacity ,Alert} from "react-native";
+import { Text, View, Image, StatusBar, FlatList, KeyboardAvoidingView, TouchableOpacity ,Alert,AppState} from "react-native";
 import { ProductitemList,AccountTabStyle } from '../../styles';
 import { useNavigation } from '@react-navigation/native';
 import { ProductData } from '../../utils/Sliderimagedata';
@@ -7,9 +7,102 @@ import { colors } from '../../utils';
 import Icon from "react-native-vector-icons/AntDesign";
 import auth from '@react-native-firebase/auth'; // Import the auth module
 import firestore from '@react-native-firebase/firestore';
+import messaging from '@react-native-firebase/messaging';
 
 
 const ProductTab = () => {
+  // useEffect(() => {
+  //   // Requesting permission to use push notifications
+  //   const requestUserPermission = async () => {
+  //     const settings = await messaging().requestPermission();
+  
+  //     if (settings) {
+  //       console.log('Permission settings:', settings);
+  //     }
+  //   };
+  
+  //   // Register background handler
+  //   messaging().setBackgroundMessageHandler(async remoteMessage => {
+  //     console.log('Message handled in the background!', remoteMessage);
+  //     // You can also display the notification here
+  //     // Alert.alert('Background Message', JSON.stringify(remoteMessage));
+  //   });
+  
+  //   // Register foreground handler for both notification and data messagesuguigiugiug
+  //   const unsubscribeForeground = messaging().onMessage(async remoteMessage => {
+  //     console.log('Message handled in the foreground!', remoteMessage);
+  //     // Display the notification if it's a notification message
+  //     if (remoteMessage.notification) {
+  //       Alert.alert(remoteMessage.notification.title, remoteMessage.notification.body);
+  //     }
+  //   });
+
+  //   requestUserPermission();
+
+  //   // Get the device token
+  //   const getDeviceToken = async () => {
+  //     const token = await messaging().getToken();
+  //     console.log('Device/**************************************************************************************** Token:', token);
+  //   };
+
+  //   getDeviceToken();
+
+  //   return () => {
+  //     unsubscribeForeground();
+  //   };
+
+  // }, []);
+  // useEffect(() => {
+  //   // Handle app state change
+  //   const handleAppStateChange = (nextAppState) => {
+  //     if (nextAppState === 'active') {
+  //       messaging()
+  //         .getInitialNotification()
+  //         .then(remoteMessage => {
+  //           if (remoteMessage) {
+  //             console.log(
+  //               'Notification caused app to open from quit state:',
+  //               remoteMessage.notification,
+  //             );
+  //             // Display the notification if it's a notification message
+  //             if (remoteMessage.notification) {
+  //               Alert.alert(remoteMessage.notification.title, remoteMessage.notification.body);
+  //             }
+  //           }
+  //         });
+  //     }
+  //   };
+
+  //   AppState.addEventListener('change', handleAppStateChange);
+
+  //   return () => {
+  //     AppState.removeEventListener('change', handleAppStateChange);
+  //   };
+  // }, []);
+  // const sendNotification = async () => {
+  //   try {
+     
+  
+  //     // Get the device token
+  //     const token = await messaging().getToken()
+      
+  //     // Construct the message
+  //     const message = {
+  //       notification: {
+  //         title: 'Title of your notification',
+  //         body: 'Body of your notification',
+  //       },
+  //       token: "cQSWf4JqSU-TijlwWOVMg9:APA91bFCHikL-Nbd2rUrvsXJAiKhSTCyGtrJq88-t-K9OI6h_8K2zWEB4Z_5p_fuDBsTxg2-kniSAybyydXaDnZyW48-mBTx2T4nwW8a4mVfM2Tfs0doqTJOtxhYKOkdfPQmKZpt3CGa",
+  //     };
+  
+  //     // Send the message
+  //     await messaging().sendMessage(message);
+  //     console.log('Notification sent successfully!');
+  //   } catch (error) {
+  //     console.log('Error sending notification:', error);
+  //   }
+  // };
+  // sendNotification()
   const pricesymboldata ='$';
   const navigation = useNavigation();
   const [hearticon, Sethearticon] = useState(0);
@@ -51,7 +144,7 @@ const ProductTab = () => {
         <View style={ProductitemList.bgwhiteboxminview2}>
           <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'center' }}>
             <TouchableOpacity style={ProductitemList.setimageviewstyle2} onPress={() => doctordata(item)}>
-              <Image style={ProductitemList.pharamacyimagestyle} resizeMode="contain" source={item.image} />
+              <Image style={ProductitemList.pharamacyimagestyle} resizeMode="contain" source={{uri:item.ImgUrl}}/>
             </TouchableOpacity>
           </View>
           <TouchableOpacity >
@@ -99,9 +192,7 @@ const ProductTab = () => {
 
   return (
     <View>
-      {/* <TouchableOpacity style={[ProductitemList.setplusbgcolorset, { backgroundColor: "#861088" }]}  >
-              <Text>add</Text>
-      </TouchableOpacity> */}
+    
       <View style={[AccountTabStyle.flexrowtwxtspace, AccountTabStyle.bgcolorset]}>
                 <Text style={AccountTabStyle.persnaltext}>All Pets</Text>
                 <TouchableOpacity onPress={() => navigation.replace('AddPetsScreen')}>
